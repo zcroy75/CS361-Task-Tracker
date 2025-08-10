5bcd7d9b13f50aa46f3808c23ccc39c2309fee35
Adam Danielson <adam.c.danielson@gmail.com>
2025-08-09 19:02:38 -0700

    Updated main.py to use CRUD operations microservice (tasks.py)

--- main.py
+++ main.py
@@ -1,70 +1,82 @@
-def guide():
+import requests
+import json
+
+
+# Microservice URLs
+CRUD_SERVICE = "http://localhost:5000"
+
+
+def print_nice(data):
+    print(json.dumps(data, indent = 2))
+
+
+def create_task():
+    name = input("Enter task name: ")
+    due_date = input("Enter task due date (YYYY-MM-DD): ")
+    task = {"task_name": name, "due_date": due_date}
+    try:
+        r = requests.post(f"{CRUD_SERVICE}/tasks", json = task)
+        print("Task created:", r.json())
+    except:
+        print("Error creating task.")
+
+
+def list_tasks():
+    try:
+        r = requests.get(f"{CRUD_SERVICE}/tasks")
+        tasks = r.json()
+        print("\n--- Tasks ---")
+        print_nice(tasks)
+    except:
+        print("Error listing tasks.")
+
+
+def delete_task():
+    check_list_choice = input("Would you like to see your task list before deleting a task? ('y' to confirm): ")
+    if check_list_choice == 'y':
+        list_tasks()
+    task_id = input("Enter task ID to delete: ")
+    try:
+        r = requests.delete(f"{CRUD_SERVICE}/tasks/{task_id}")
+        print("Response:", r.json())
+    except:
+        print("Error deleting task.")
+
+
+def menu():
     # Print available commands to user
-    print("COMMANDS:")
-    print("Type 'add' to add a task to the list.")
-    print("Type 'view' to see the full task list.")
-    print("Type 'delete' to remove a task from the list.")
-    print("Type 'quit' to exit the program.\n")
+    print("\n=== To-Do App Menu ===")
+    print("1. Create Task")
+    print("2. List Tasks")
+    print("3. Delete Task")
+    print("0. Exit")
 
     # Prompt user for action
-    return input("What would you like to do?\n")
-
-
-def add(task_list):
-    task = input("Please write your task below:\n")
-    task_list.append(task)
-    print("")
-    return task_list
-
-
-def view(task_list):
-    if task_list == []:
-        return print("The list of tasks is empty! Add tasks by using the 'add' command.\n")
-    index = 1
-    for task in task_list:
-        print(index, ": ", task, sep='')
-        index += 1
-    print("")
-    return
-
-
-def delete(task_list):
-    confirm_choice = input("Be aware that removing a task is final. Type 'confirm' if you would still like to continue, type anything else if you no longer want to delete a task: ")
-    if confirm_choice != 'confirm':
-        print("No tasks have been removed from the list.\n")
-        return task_list
-    view_choice = input("Would you like to see the list of your tasks first? Type 'yes' to confirm, type anything else to go straight to deleting a task: ")
-    if view_choice == 'yes':
-        print("Here are your tasks:")
-        view(task_list)
-    choice = int(input("Please provide the number of the task you would like to remove, or type 'cancel' if you have decided not to remove a task: "))
-    if choice <= len(task_list):
-        task_list.pop(choice - 1)
-        print("Your task has been successfully removed from the list.\n")
-    else:
-        print("You entered an invalid number, please double check your task list and try again.\n")
-    return task_list
+    return input("\nChoose an option: ")
 
 
 def main():
     # Introduce program to user
     print("Welcome, user!")
     print("This program will keep track of your tasks as a list.")
-    print("Newest features of this program include the ability to remove tasks from your list.")
-    print("Please view the COMMANDS section below for more info on available features.\n")
+    print("Functions include creating, viewing, and deleting tasks.")
+    print("Please choose a feature from the section below.")
 
-    task_list = []
     while True:
-        choice = guide()
+        choice = menu()
         print("")
         match choice:
-            case 'add':
-                task_list = add(task_list)
-            case 'view':
-                view(task_list)
-            case 'delete':
-                task_list = delete(task_list)
-            case 'quit':
+            # Create task
+            case '1':
+                create_task()
+            # View tasks
+            case '2':
+                list_tasks()
+            # Delete task
+            case '3':
+                delete_task()
+            # Exit program
+            case '0':
                 quit_choice = input("Are you sure you want to exit the program? Type 'yes' to confirm, type anything else to stay: ")
                 if quit_choice == 'yes':
                     print("Thank you for using the task tracker program!\n")
